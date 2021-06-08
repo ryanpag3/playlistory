a7ee945933e421913498c87d7f9abb84dbad934c
Ryan Page <ryanapage@gmail.com>
2021-06-08 08:22:38 -0700

    add fastify-swagger to project

--- api/src/index.test.ts
+++ api/src/index.test.ts
@@ -2,13 +2,14 @@ import { delay } from 'bluebird';
 import logger from './util/logger';
 it('should start the server', async () => {
     try {
+        logger.info('testing');
         const server = require('./index');
         await delay(1000);
         const isInit = server.isServerReady();
         expect(isInit).toBeTruthy();
         await server.server.close();
     } catch (e) {
-        console.log(e);
+        logger.error(e);
         throw e;
     }
 }, 5000);
