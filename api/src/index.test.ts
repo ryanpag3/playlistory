392f747c0dadb87341a8ba8646ccaa80dd704a9d
Ryan Page <ryanapage@gmail.com>
2021-06-08 08:35:00 -0700

    add comment for context regarding logger weirdness

--- api/src/index.test.ts
+++ api/src/index.test.ts
@@ -2,14 +2,14 @@ import { delay } from 'bluebird';
 import logger from './util/logger';
 it('should start the server', async () => {
     try {
-        logger.info('testing');
         const server = require('./index');
         await delay(1000);
         const isInit = server.isServerReady();
         expect(isInit).toBeTruthy();
         await server.server.close();
     } catch (e) {
-        logger.error(e);
+        // when using require for some reason logger. calls werent working on error
+        console.log(e);
         throw e;
     }
 }, 5000);
