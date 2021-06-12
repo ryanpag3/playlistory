8d68134160c6a29748dba313d864b358f2b0f8f1
Ryan Page <ryanapage@gmail.com>
2021-06-12 13:25:47 -0700

    fix input validation effects not firing

--- web/src/screens/SignUp/SignUpForm.tsx
+++ web/src/screens/SignUp/SignUpForm.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components';
 import Input from '../../components/Input';
 import colors from '../../constants/colors';
 
 const SignUpForm = () => {
+    const [email, setEmail] = useState();
+    const [confirmEmail, setConfirmEmail] = useState();
+    const [password, setPassword] = useState();
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        console.log('fuck')
+        if (email !== confirmEmail) {
+            setError('Emails must match.' as any);
+        } else {
+            setError(undefined as any);
+        }
+    }, [ email, confirmEmail ]);
+
     return (
         <Container>
             <TitleContainer>
@@ -12,21 +26,30 @@ const SignUpForm = () => {
             <EmailInput
                 placeholder="email"
                 onChange={(t) => {
-                    console.log(t);
+                    setEmail(t.target.value);
                 }}
             />
-            <EmailInput
-                placeholder="email"
-                onChange={(t) => {
-                    console.log(t);
-                }}
+            <ConfirmEmailInput
+                placeholder="confirm email"
+                onChange={(t) => setConfirmEmail(t.target.value)}
             />
-            <EmailInput
-                placeholder="email"
-                onChange={(t) => {
-                    console.log(t);
-                }}
+            <PasswordInput
+                placeholder="password"
+                onChange={(t) => setPassword(t.target.value)}
+                type="password"
             />
+            <SubmitContainer>
+                <SubmitButton>
+                    Submit
+                </SubmitButton>
+                <LoginAnchor>
+                    Login
+                </LoginAnchor>
+            </SubmitContainer>
+            {
+                error &&
+                <ErrorMsg>{error}</ErrorMsg>
+            }
         </Container>
     )
 }
@@ -36,7 +59,7 @@ const Container = styled.div`
     flex-direction: column;
     background-color: ${colors.PRIMARY_ACCENT};
     width: 300px;
-    height: 350px;
+    height: 375px;
     border-radius: 30px;
     align-items: center;
 `;
@@ -48,18 +71,47 @@ const TitleContainer = styled.div`
 const Title = styled.h1`
     color: ${colors.LIGHT};
     font-size: 2.5em;
+    margin-bottom: .5em;
 `;
 
-const EmailInput = styled(Input)`
+const StyledInput = styled(Input)`
+    margin-top: 10px;
+`;
+
+const EmailInput = styled(StyledInput)`
 
 `;
 
-const ConfirmEmailInput = styled(Input)`
+const ConfirmEmailInput = styled(StyledInput)`
 
 `;
 
-const PasswordInput = styled(Input)`
+const PasswordInput = styled(StyledInput)`
 
+`;
+
+const SubmitContainer = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+`;
+
+const SubmitButton = styled.button`
+    margin-top: 20px;
+    font-size: 16px;
+    padding: 8px;
+    border-radius: 10px;
+    border: none;
+    background-color: ${colors.SECONDARY_ACCENT};
+    min-width: 100px;
+`;
+
+const LoginAnchor = styled.a`
+    margin-top: 20px;
+`;
+
+const ErrorMsg = styled.h2`
+    color: red;
 `;
 
 export default SignUpForm
