540bf3c33ad522df5265d822ee24e4b67d889677
Ryan Page <ryanapage@gmail.com>
2021-06-16 11:17:55 -0700

    attach to any available port

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,8 +5,10 @@ import logger from './util/logger';
 
 export const server = Fastify({});
 
+const port = process.env.NODE_ENV === 'test' ? 0 : process.env.API_PORT || 3000;
+
 setupServer(server)
-    .then(() => server.listen(process.env.PORT || 3000, (err, address) => {
+    .then(() => server.listen(port, (err, address) => {
         logger.info(`Playlistory API server has been started on ${address}`);
     }));
 
