c9a314f0717f09d8433ffcbf8b2ad2ec562ffe4c
Ryan Page <ryanapage@gmail.com>
2021-06-17 08:18:09 -0700

    add test case for backup service

--- api/src/backup/backup-service.test.ts
+++ api/src/backup/backup-service.test.ts
@@ -1,7 +1,9 @@
+import { delay } from 'bluebird';
 import Platforms from '../../../shared/src/Platforms';
-import { createUser } from '../user/user-controller'
 import { create } from '../user/user-service'
-import { createBackup, getMostRecentBackup } from './backup-service'
+import logger from '../util/logger';
+import { getRandomString } from '../util/test-helper';
+import { createBackup, generateManifest, getMostRecentBackup } from './backup-service'
 
 it('should create a backup', async () => {
     const user = await create({
@@ -55,4 +57,35 @@ it('should get the most recent backup', async () => {
     });
     const recent = await getMostRecentBackup('1234');
     expect(recent?.id).toBe(backup.id);
+});
+
+it('should generate a manifest for two backups', async () => {
+    // these are randomly generated so should give a high probability of diff
+    // **technically** they have different owners but its irrelevant for this
+    const backup1 = await generateBackup();
+    const backup2 = await generateBackup();
+    const withManifest = await generateManifest(backup1, backup2);
+    expect(withManifest.manifest).not.toBeNull();
 })
+
+const generateBackup = async () => {
+    const user = await create({
+        email: getRandomString(),
+        password: getRandomString()
+    });
+
+    const backup = await createBackup(user, {
+        platform: Platforms.SPOTIFY,
+        playlistId: getRandomString(),
+        imageUrl: getRandomString(),
+        name: getRandomString(),
+        playlistName: getRandomString(),
+        playlistDescription: getRandomString(),
+        contentHash: getRandomString(),
+        tracks: [getRandomString()],
+        followers: 0,
+        createdById: user.id
+    });
+
+    return backup;
+}
