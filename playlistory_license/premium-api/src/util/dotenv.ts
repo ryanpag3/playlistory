deeae23eceb566482083b5ab1944e82e1e7c4603
Ryan Page <ryanapage@gmail.com>
2021-07-19 07:27:52 -0700

    use per-file licensing model instead of a top level directory

--- playlistory_license/premium-api/src/util/dotenv.ts
+++
@@ -1,5 +0,0 @@
-const dotenv = require('dotenv');
-dotenv.config();
-dotenv.config({
-    path: '.env.private'
-});
--- playlistory_license/premium-api/src/util/logger.ts
+++
@@ -1,7 +0,0 @@
-import log4js from 'log4js';
-
-const logger = log4js.getLogger();
-
-logger.level = process.env.LOG_LEVEL || 'INFO';
-
-export default logger;
