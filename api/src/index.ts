bd93c48f18646e626ec3d8e2c68df9a6bceef776
Ryan Page <ryanapage@gmail.com>
2021-08-03 17:04:34 -0700

    make address configurable

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,10 +5,12 @@ import logger from './util/logger';
 
 export const server = Fastify({});
 
+const address = process.env.API_ADDRESS || '127.0.0.1';
+
 const port = process.env.NODE_ENV === 'test' ? 0 : process.env.API_PORT || 3000;
 
 setupServer(server)
-    .then(() => server.listen(port, (err, address) => {
+    .then(() => server.listen(port, address, (err, address) => {
         logger.info(`Playlistory API server has been started on ${address}`);
     }));
 
